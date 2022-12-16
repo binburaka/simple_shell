cef8c60a8e927167cff72d837560aee906fec911
Obinna Robinson <obinnarobinson1@gmail.com>
2022-12-16 12:01:06 -0800

    shell project

--- shell_loop.c
+++ shell_loop.c
@@ -1,4 +1,4 @@
-include "shell.h"
+#include "shell.h"
 
 /**
  * hsh - main shell loop
@@ -106,7 +106,9 @@ void find_cmd(info_t *info)
 	else
 	{
 		if ((interactive(info) || _getenv(info, "PATH=")
-					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+		|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
+
 		else if (*(info->arg) != '\n')
 		{
 			info->status = 127;
